a3f2632d38ec38b0a30a97b74644988272fd81a3
ilya pribil <ilya@casualgamesonline.com>
2026-01-23 10:42:46 +0500

    WIP

--- playgama-bridge-unity.js
+++ playgama-bridge-unity.js
@@ -135,14 +135,14 @@ function initializeBridge() {
             bridge.platform.on('pause_state_changed', isPaused => sendMessageToUnity('OnPauseStateChanged', isPaused.toString()))
 
             let unityLoader = document.createElement('script')
-            unityLoader.src = 'Build/Build 3.loader.js'
+            unityLoader.src = 'Build/Build 4.loader.js'
             unityLoader.onload = () => {
                 createUnityInstance(
                     CANVAS,
                     {
-                        dataUrl: 'Build/Build 3.data.unityweb',
-                        frameworkUrl: 'Build/Build 3.framework.js.unityweb',
-                        codeUrl: 'Build/Build 3.wasm.unityweb',
+                        dataUrl: 'Build/Build 4.data.unityweb',
+                        frameworkUrl: 'Build/Build 4.framework.js.unityweb',
+                        codeUrl: 'Build/Build 4.wasm.unityweb',
                         streamingAssetsUrl: 'StreamingAssets',
                         companyName: 'DefaultCompany',
                         productName: 'MatchImages',
