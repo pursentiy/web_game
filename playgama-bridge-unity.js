cb054f89990b37177f5485f8e3372a2abf9e9036
ilya pribil <ilya@casualgamesonline.com>
2026-01-27 21:45:53 +0500

    wip

--- playgama-bridge-unity.js
+++ playgama-bridge-unity.js
@@ -135,14 +135,14 @@ function initializeBridge() {
             bridge.platform.on('pause_state_changed', isPaused => sendMessageToUnity('OnPauseStateChanged', isPaused.toString()))
 
             let unityLoader = document.createElement('script')
-            unityLoader.src = 'Build/Build Prod 13.loader.js'
+            unityLoader.src = 'Build/Build Prod 14.loader.js'
             unityLoader.onload = () => {
                 createUnityInstance(
                     CANVAS,
                     {
-                        dataUrl: 'Build/Build Prod 13.data.unityweb',
-                        frameworkUrl: 'Build/Build Prod 13.framework.js.unityweb',
-                        codeUrl: 'Build/Build Prod 13.wasm.unityweb',
+                        dataUrl: 'Build/Build Prod 14.data.unityweb',
+                        frameworkUrl: 'Build/Build Prod 14.framework.js.unityweb',
+                        codeUrl: 'Build/Build Prod 14.wasm.unityweb',
                         streamingAssetsUrl: 'StreamingAssets',
                         companyName: 'Vopris.inc',
                         productName: 'Magic Puzzles',
